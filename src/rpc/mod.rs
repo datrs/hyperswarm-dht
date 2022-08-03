11dff4ee58e540b08511f7c659181fefa7bb4d58
peg <peg@magmacollective.org>
2022-08-03 15:46:56 +0200

    Change default bind address to the unspecified address 0.0.0.0

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -259,7 +259,7 @@ impl RpcDht {
             socket
         } else {
             UdpSocket::bind(SocketAddr::V4(SocketAddrV4::new(
-                Ipv4Addr::new(127, 0, 0, 1),
+                Ipv4Addr::new(0, 0, 0, 0),
                 0,
             )))
             .await?
